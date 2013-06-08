0908994da0ca4c56d8ef40de9694dd9de7abfb62
Ren Wei (任卫) <wadefelix@gmail.com>
2013-06-08 13:11:07 +0800

    after php5.3, this is the default mode.

--- actions/rdf.php
+++ actions/rdf.php
@@ -2,7 +2,7 @@
 require_once "XML/RSS.php";
 $surl=$action_params['url'];
 $surl=$this->StripQuotes($surl);
-$rss =& new XML_RSS($surl);
+$rss = new XML_RSS($surl);
 $rss->parse();
 echo "<ul>\n";
 foreach ($rss->getItems() as $item) {
