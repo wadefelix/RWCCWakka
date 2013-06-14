cb87a5e707f7c83eab1f22fb1d568a534335dedd
Ren Wei (任卫) <wadefelix@gmail.com>
2013-06-14 13:08:49 +0800

    convert the configed charset character to GBK(?) to surport the simpled chinese windows

--- actions/files.php
+++ actions/files.php
@@ -36,20 +36,27 @@
         echo "<a href=\"".$this->href('files.xml', $this->GetPageTag(), 'action=download&file='.urlencode($this->stripquotes($action_params['download'])))."\">".$text."</a>";
     } elseif ($this->page AND $this->page['latest']=="Y" && $this->HasAccess('write') AND ($this->method <> 'print.xml') AND ($this->method <> 'edit')) {
          
-        if (! is_dir($upload_path))
-            mkdir_r($upload_path);
+        if (! is_dir(iconv($this->config["charset"], "GBK",$upload_path)))
+            mkdir_r(iconv($this->config["charset"], "GBK",$upload_path));
          
         // upload action
         $uploaded = $_FILES['file'];
         if ($_REQUEST['action'] == 'upload' AND $uploaded['size'] > 0) {
-	    if (file_exists($upload_path.'/'.$uploaded['name'])){
-	    	$this->SetMessage(_MI_FILE_EXIST);
-		$this->Redirect($this->Href());
-		}
+            if($uploaded['size'] > $this->config["AllowUploadMaxFileSize"])
+            {
+                $this->SetMessage("Sorry, filesize not allowed.");
+		        $this->Redirect($this->Href());
+            }
+	        if (file_exists($upload_path.'/'.$uploaded['name'])){
+	    	    $this->SetMessage(_MI_FILE_EXIST);
+		        $this->Redirect($this->Href());
+		    }
             if (check_ext($uploaded['name'])){
-            copy ($uploaded['tmp_name'], $upload_path.'/'.$uploaded['name']);
-	    $this->LogOperation("_OP_FILE_UPLOAD",$this->GetPageTag(),$uploaded['name']);
-	    }
+                $dest = $upload_path.'/'.$uploaded['name'];
+                move_uploaded_file ($uploaded['tmp_name'],iconv($this->config["charset"], "GBK", $dest ));
+                //copy ($uploaded['tmp_name'], $upload_path.'/'.$uploaded['name']);
+	            $this->LogOperation("_OP_FILE_UPLOAD",$this->GetPageTag(),$uploaded['name']);
+	        }
             else
                 //echo "Sorry, the site doesn't allow you to upload this type of files!(".$uploaded['name'].")";
             echo _MI_ERROR_FILETYPE."(".$uploaded['name'].")";
@@ -70,13 +77,14 @@
     if ($this->HasAccess('read')) {
         // uploaded files
         //print($this->Format("----"));
-        $dir = opendir($upload_path);
+        $dir = opendir(iconv($this->config["charset"], "GBK",$upload_path));
         while ($file = readdir($dir)) {
+            $file_utf8 = iconv("GBK",$this->config["charset"], $file);
             if ($file != '.' && $file != '..') {
-                $delete_link = "<a href=\"".$this->href('files.xml', $this->GetPageTag(), 'action=delete&file='.urlencode($file))."\">x</a>";
-                $download_link = "<a href=\"".$this->href('files.xml', $this->GetPageTag(), 'action=download&file='.urlencode($file))."\">".$file."</a>";
+                $delete_link = "<a href=\"".$this->href('files.xml', $this->GetPageTag(), 'action=delete&file='.urlencode($file_utf8))."\">x</a>";
+                $download_link = "<a href=\"".$this->href('files.xml', $this->GetPageTag(), 'action=download&file='.urlencode($file_utf8))."\">".$file_utf8."</a>";
                 if ($this->IsManager())print "[ {$delete_link} ] ";
-                if ($file == $uploaded['name'])
+                if ($file_utf8 == $uploaded['name'])
                     print "<em>{$download_link}</em>\n";
                 else
                     print $download_link;
