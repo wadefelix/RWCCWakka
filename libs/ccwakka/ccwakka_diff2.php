ac305d0a7a7ab634bd8e485a6796dea1cd0a3ded
renwei <renwei@renwei.net>
2020-02-26 09:42:55 +0800

    php7 的构造函数

--- libs/ccwakka/ccwakka_diff2.php
+++ libs/ccwakka/ccwakka_diff2.php
@@ -30,7 +30,7 @@ class _DiffOp {
 class _DiffOp_Copy extends _DiffOp {
     var $type = 'copy';
     
-    function _DiffOp_Copy ($orig, $closing = false) {
+    function __construct ($orig, $closing = false) {
         if (!is_array($closing))
             $closing = $orig;
         $this->orig = $orig;
@@ -45,7 +45,7 @@ class _DiffOp_Copy extends _DiffOp {
 class _DiffOp_Delete extends _DiffOp {
     var $type = 'delete';
     
-    function _DiffOp_Delete ($lines) {
+    function __construct ($lines) {
         $this->orig = $lines;
         $this->closing = false;
     }
@@ -58,7 +58,7 @@ class _DiffOp_Delete extends _DiffOp {
 class _DiffOp_Add extends _DiffOp {
     var $type = 'add';
     
-    function _DiffOp_Add ($lines) {
+    function __construct ($lines) {
         $this->closing = $lines;
         $this->orig = false;
     }
@@ -71,7 +71,7 @@ class _DiffOp_Add extends _DiffOp {
 class _DiffOp_Change extends _DiffOp {
     var $type = 'change';
     
-    function _DiffOp_Change ($orig, $closing) {
+    function __construct ($orig, $closing) {
         $this->orig = $orig;
         $this->closing = $closing;
     }
@@ -501,7 +501,7 @@ class Diff
      *        (Typically these are lines from a file.)
      * @param $to_lines array An array of strings.
      */
-    function Diff($from_lines, $to_lines) {
+    function __construct ($from_lines, $to_lines) {
         $eng = new _DiffEngine;
         $this->edits = $eng->diff($from_lines, $to_lines);
         //$this->_check($from_lines, $to_lines);
@@ -650,7 +650,7 @@ extends Diff
      * @param $mapped_to_lines array This array should
      *  have the same number of elements as $to_lines.
      */
-    function MappedDiff($from_lines, $to_lines,
+    function __construct ($from_lines, $to_lines,
                         $mapped_from_lines, $mapped_to_lines) {
 
         assert(sizeof($from_lines) == sizeof($mapped_from_lines));
@@ -837,7 +837,7 @@ class DiffFormatter
 define('NBSP', "\xA0");         // iso-8859-x non-breaking space.
 
 class _HWLDF_WordAccumulator {
-    function _HWLDF_WordAccumulator () {
+    function __construct () {
         $this->_lines = array();
         $this->_line = '';
         $this->_group = '';
@@ -888,7 +888,7 @@ class _HWLDF_WordAccumulator {
 
 class WordLevelDiff extends MappedDiff
 {
-    function WordLevelDiff ($orig_lines, $closing_lines) {
+    function __construct ($orig_lines, $closing_lines) {
         list ($orig_words, $orig_stripped) = $this->_split($orig_lines);
         list ($closing_words, $closing_stripped) = $this->_split($closing_lines);
 
@@ -940,7 +940,7 @@ class WordLevelDiff extends MappedDiff
  */
 class UnifiedDiffFormatter extends DiffFormatter
 {
-    function UnifiedDiffFormatter($context_lines = 4) {
+    function __construct ($context_lines = 4) {
         $this->leading_context_lines = $context_lines;
         $this->trailing_context_lines = $context_lines;
     }
@@ -971,7 +971,7 @@ class UnifiedDiffFormatter extends DiffFormatter
  */
 class TableDiffFormatter extends DiffFormatter
 {
-    function TableDiffFormatter() {
+    function __construct () {
         $this->leading_context_lines = 2;
         $this->trailing_context_lines = 2;
     }
